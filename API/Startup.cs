ee7d15bed7e32c24a56f7828904f6fcae7636503
Minh-Hoangg <mhoangkt8@gmail.com>
2023-04-22 15:53:16 +0700

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
             });
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => {
+                x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+            });
 
             app.UseAuthorization();
 
